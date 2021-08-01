faea68383981f33da641f859abd108682febbfb9
BlueNickel <work.lasse.face@gmail.com>
2021-08-02 01:49:48 +0200

    finished API

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const http = require('http');
+import express, {Router} from "express"
+import cors from 'cors'
+import webhook from 'discord-webhook-node'
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const hook = new webhook.Webhook("https://discord.com/api/webhooks/871517501127794789/tPy69h0f7IzoJ7TQMFCzQnneF7Odulol_fUqJPJDJOcCuJUNxumf-Dy2oLJtyG3pYBX8")
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+const app = express()
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(cors())
+
+const bgRouter = Router()
+const port = process.env.PORT || 25569
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+app.post('/emailsubmit', (req, res) => {
+    hook.info('**Contact Form**', 'New Submit Form', `${req.body.fullName} \n ${req.body.email} \n ${req.body.subject} \n \n ${req.body.message}`)
+    console.log(req.body)
+    res.send("success")
+})
+
+app.get('/testing', (req, res) => {
+    res.send('test successful')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
